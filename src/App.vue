199919d7fcedb85a700d8637b125fa2ac5510760
Cesar Flores <cesar.flores@gmail.com>
2025-09-05 16:02:53 -0600

    Inicio del proyecto

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <div class="text-center pt-3 text-success h3">JuegoDeContador</div>
+    <hr>
+  </header>
+
+
+</template>
+
+<style scoped></style>
