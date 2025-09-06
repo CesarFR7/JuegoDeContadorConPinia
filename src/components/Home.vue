ac382d8a244e8ceecb93480e13ab7597f9efa000
Cesar Flores <cesar.flores@gmail.com>
2025-09-05 18:16:46 -0600

    Se agregaron componentes Home e info

---
+++ src/components/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="text-center">
+        <p>Let´s play the game</p>
+        <h2 class="text-primary pb-3">Current Score</h2>
+        <span class="text-primary pb-3">Max Score</span>
+    </div>
+
+</template>
