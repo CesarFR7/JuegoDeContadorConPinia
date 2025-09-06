898e68e7a53e16f378707342b248b79d5e24edf1
Cesar Flores <cesar.flores@gmail.com>
2025-09-05 18:59:02 -0600

    Se agregó el Reuter

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,17 @@ import { RouterLink, RouterView } from 'vue-router';
   <header>
     <div class="text-center pt-3 text-success h3">JuegoDeContador</div>
     <hr>
-  </header>
+    <div class="row">
+      <div class="col-4 offset-1">
+        <router-link to="/" class="form-control badge rounded-pill text-bg-warning p-4">Home</router-link>
+      </div>
+      <div class="col-4 offset-1">
+        <router-link to="/info" class="form-control badge rounded-pill text-bg-warning p-4">Info</router-link>
+      </div>
+    </div>
 
+  </header>
+  <RouterView></RouterView>
 
 </template>
 
